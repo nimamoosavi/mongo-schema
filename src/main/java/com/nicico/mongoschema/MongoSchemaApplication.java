f8083c10375f360d519cf7cd8885061a1572b3e4
hossein <mahdevarhossein@gmail.com>
2021-06-09 11:37:51 +0430

    init

---
+++ src/main/java/com/nicico/mongoschema/MongoSchemaApplication.java
@@ -0,0 +1,13 @@
+package com.nicico.mongoschema;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MongoSchemaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MongoSchemaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nicico/mongoschema/MongoSchemaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nicico.mongoschema;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MongoSchemaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
